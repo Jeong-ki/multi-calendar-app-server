76c5778695e302be1541e665daebd9e1abde45c9
Jeong-ki <7863z@naver.com>
2024-02-20 22:52:02 +0900

    implepent auth validators

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { signInUser, signUpUser, myInfo } from "../controllers/authController";
 import { isAuth } from "../utils/auth";
+import {
+  signInValidationRules,
+  signUpValidationRules,
+  validate,
+} from "../utils/validators/userValidator";
 
 const router = express.Router();
 
-router.post("/signUp", signUpUser);
-router.post("/signIn", signInUser);
+router.post("/signUp", signUpValidationRules(), validate, signUpUser);
+router.post("/signIn", signInValidationRules(), validate, signInUser);
 router.get("/myInfo", isAuth, myInfo);
 
 export default router;
---
+++ src/utils/validators/userValidator.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { body, ValidationChain, validationResult } from "express-validator";
+
+export const signUpValidationRules = (): ValidationChain[] => [
+  body("email").isEmail().withMessage("Enter a valid email address"),
+  body("username")
+    .isLength({ min: 2 })
+    .withMessage("Username must be at least 2 characters long"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long"),
+];
+
+export const signInValidationRules = (): ValidationChain[] => [
+  body("email").isEmail().withMessage("Enter a valid email address"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long"),
+];
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  res.status(400).json({ errors: errors.array() });
+};
